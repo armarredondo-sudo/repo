b2a7736f0827bc5c5d2b80891dfcf680b09cdaea
armarredondo-sudo <arm.arredondo@duocuc.cl>
2026-06-12 16:59:17 -0400

    isiu

---
+++ ayudantia 4 steam/funciones.py
@@ -0,0 +1,26 @@
+def mostrar_menu():
+
+    print(""" ● Opción 1: Ver Catálogo de Tienda 
+          
+● Opción 2: Agregar juego al Carrito 
+          
+● Opción 3: Ver mi Carrito
+          
+● Opción 4: Cargar fondos a la Cartera 
+          
+● Opción 5: Pagar Carrito
+          
+● Opción 6: Ver mi Biblioteca 
+          
+● Opción 7: Salir           
+""")
+    
+def mostrar_juegos(lista_generica):
+   if len(lista_generica) == 0:
+    print("no hay nada en la lista")
+   else:
+    print
+
+def buscar_juego (catalalogo, nombre_buscado):
+  for juego in catalalogo:
+    if juego ["titulo"].lover() == nombre_buscado.lover():
